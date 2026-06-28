61dff0b28aaaa2d0362ed31e5ccc8d29ce8692ac
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-06-28 23:46:08 +0530

    implement DisplayNameDemo class for custom display names

---
+++ src/test/java/junit/DisplayNameDemo.java
@@ -0,0 +1,21 @@
+package junit;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("A special test case")
+public class DisplayNameDemo {
+    @Test
+    @DisplayName("Custom test name containing spaces")
+    void testWithDisplayNameContainingSpaces() {
+    }
+    @Test
+    @DisplayName("╯°")
+    void testWithDisplayNameContainingSpecialCharacters() {
+    }
+    @Test
+    @DisplayName("😂😂😘")
+
+    void testWithDisplayNameContainingEmoji() {
+    }
+}

ef933bc458a586377a0092d01e5a30b89b17529a
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-06-29 10:11:34 +0530

    implement AssertionsDemo

--- src/main/java/junit/Calculate.java
+++ src/main/java/junit/Calculate.java
@@ -5,4 +5,14 @@ public class Calculate {
 		System.out.println("Adding values: " + var1 + " + " + var2);
 		return var1 + var2;
 	}
+
+	public int multiply(int var1, int var2) {
+		System.out.println("Multiplying values: " + var1 + " * " + var2);
+		return var1 * var2;
+	}
+
+	public int divide(int var1, int var2) {
+		System.out.println("Dividing values: " + var1 + " / " + var2);
+		return var1/var2;
+	}
 }
--- src/test/java/junit/DisplayNameDemo.java
+++ src/test/java/junit/DisplayNameDemo.java
@@ -15,7 +15,6 @@ public class DisplayNameDemo {
     }
     @Test
     @DisplayName("😂😂😘")
-
     void testWithDisplayNameContainingEmoji() {
     }
 }

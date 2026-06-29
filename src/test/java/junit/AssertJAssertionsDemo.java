4d761f415d37e9c50b498eba5f50150040e66137
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-06-29 22:36:49 +0530

    add Object Assertions test methods to AssertJAssertionsDemo class

--- src/test/java/junit/AssertJAssertionsDemo.java
+++ src/test/java/junit/AssertJAssertionsDemo.java
@@ -7,8 +7,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AssertJAssertionsDemo {
     private final Calculate  calculator = new Calculate();
+    private final Person person1 = new Person("suranga","madhushan");
+    private final Person person2 = new Person("suranga","madhushan");
     @Test
     void assertWithAssertJ() {
         assertThat(calculator.sum(4, 1)).isEqualTo(5);
+    }
+    @Test
+    void compareObject(){
+        assertThat(person1).isEqualTo(person2);
+    }
+
+    @Test
+    void compareObjectFields(){
+        assertThat(person1)
+                .usingRecursiveComparison()
+                .isEqualTo(person2);
     }
 }
